df286ac4e4d7957dbae8b926843bca24c4fbf453
Ganesh @ PCTEST <35346980+garva-pctest@users.noreply.github.com>
2018-01-17 13:13:51 -0500

    Keyboard support + image in center of controls

--- manual_gui.py
+++ manual_gui.py
@@ -6,7 +6,7 @@
 import Tkinter as tk
 from tkFont import Font
 from motor_driver import MotorDriver
-
+from PIL import ImageTk, Image
 
 class ManualGUI(tk.Tk):
     """Tkinter GUI that allows user to move motors manually set distances.
@@ -27,18 +27,14 @@ class ManualGUI(tk.Tk):
         tk.Tk.__init__(self, parent)
         self.parent = parent
         self.motor = MotorDriver()
-        self.set_variables(x_dist_, y_dist_)
-        self.x_error, self.y_error = 0, 0
-        self.x_loc, self.y_loc = 0, 0
-        self.setup()
-
-    def set_variables(self, x_dist_, y_dist_):
-        self.x_dist = x_dist_
-        self.y_dist = y_dist_
+        self.x_dist, self.y_dist = x_dist_, y_dist_
         self.x_steps = int(self.x_dist / self.motor.step_unit)
         self.y_steps = int(self.y_dist / self.motor.step_unit)
         self.x_frac = self.x_dist / self.motor.step_unit - self.x_steps
         self.y_frac = self.y_dist / self.motor.step_unit - self.y_steps
+        self.x_error, self.y_error = 0, 0
+        self.x_loc, self.y_loc = 0, 0
+        self.setup()
 
     def update_variables(self):
         if float(self.x_entry.get()) < 0 or float(self.y_entry.get()) < 0:
@@ -50,13 +46,14 @@ class ManualGUI(tk.Tk):
         self.y_steps = int(self.y_dist / self.motor.step_unit)
         self.x_frac = self.x_dist / self.motor.step_unit - self.x_steps
         self.y_frac = self.y_dist / self.motor.step_unit - self.y_steps
-        print self.x_dist, self.y_dist
+        print 'Distances changed: X =', self.x_dist, 'Y =', self.y_dist
 
     def setup(self):
         self.title('Please select a location on the grid')
         self.config(background='lightblue')
-        my_font = Font(family='Sans Serif', size=15)
+        my_font = Font(family='Nirmala UI', size=15)
 
+        # Button control grid
         controlFrame = tk.Frame(background='lightblue', padx=10, pady=30)
         btnUp = tk.Button(controlFrame, text='Up', command=self.selectedUp, padx=15, pady=20, bg='cyan', font=my_font)
         btnDown = tk.Button(controlFrame, text='Down', command=self.selectedDown, padx=15, pady=20, bg='cyan',
@@ -65,20 +62,29 @@ class ManualGUI(tk.Tk):
                             font=my_font)
         btnRight = tk.Button(controlFrame, text='Right', command=self.selectedRight, padx=20, pady=20, bg='cyan',
                              font=my_font)
+
+        img = Image.open('images/EHP-200A-500x500.gif').rotate(180)
+        img.thumbnail((50, 50), Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(img)
+        lab = tk.Label(controlFrame, image=photo)
+        lab.image = photo
         btnUp.grid(row=0, column=1, sticky='NSEW')
         btnDown.grid(row=2, column=1, sticky='NSEW')
         btnLeft.grid(row=1, column=0, sticky='NSEW')
         btnRight.grid(row=1, column=2, sticky='NSEW')
+        lab.grid(row=1, column=1)
 
         controlFrame.grid_rowconfigure(3, weight=1)
         controlFrame.grid_columnconfigure(3, weight=1)
-        controlFrame.grid(row=0, column=0)
+        controlFrame.grid(row=0, column=0, padx=10, pady=10)
 
+        # Display the location of the robot relative to the start position
         self.loc = tk.Label(self, text='Current location:\n[%.3f, %.3f]' % (self.x_loc, self.y_loc), font=my_font,
-                            padx=20, pady=20)
-        self.loc.grid(row=1, column=0, sticky='NSEW')
+                            padx=20, pady=20, bg='white')
+        self.loc.grid(row=1, column=0, sticky='NSEW', padx=150, pady=10)
 
-        modifyFrame = tk.Frame(background='lightblue')
+        # For changing the set distances mid-program
+        modifyFrame = tk.Frame(bg='lightblue')
         self.x_entry = tk.Entry(modifyFrame, font=my_font)
         self.y_entry = tk.Entry(modifyFrame, font=my_font)
         self.x_entry.insert(0, str(self.x_dist))
@@ -101,29 +107,35 @@ class ManualGUI(tk.Tk):
         self.grid_rowconfigure(3, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
-    def selectedUp(self):
+        # Keyboard controls
+        self.bind('<Control-Up>', self.selectedUp)
+        self.bind('<Control-Down>', self.selectedDown)
+        self.bind('<Control-Left>', self.selectedLeft)
+        self.bind('<Control-Right>', self.selectedRight)
+
+    def selectedUp(self, event):
         self.y_loc -= self.y_dist
         self.y_error -= self.y_frac
         self.motor.reverse_motor_two(self.y_steps + int(self.y_error))
         self.y_error -= int(self.y_error)
         self.loc.config(text='Current location:\n[%.3f, %.3f]' % (self.x_loc, self.y_loc))
 
-    def selectedDown(self):
+    def selectedDown(self, event):
         self.y_loc += self.y_dist
         self.y_error += self.y_frac
         self.motor.forward_motor_two(self.y_steps + int(self.y_error))
         self.y_error -= int(self.y_error)
         self.loc.config(text='Current location:\n[%.3f, %.3f]' % (self.x_loc, self.y_loc))
 
-    def selectedLeft(self):
+    def selectedLeft(self, event):
         self.x_loc -= self.x_dist
         self.x_error -= self.x_frac
         self.motor.reverse_motor_one(self.x_steps + int(self.x_error))
         self.x_error -= int(self.x_error)
         self.loc.config(text='Current location:\n[%.3f, %.3f]' % (self.x_loc, self.y_loc))
 
-    def selectedRight(self):
-        self.x_loc += self.y_dist
+    def selectedRight(self, event):
+        self.x_loc += self.x_dist
         self.x_error += self.x_frac
         self.motor.forward_motor_one(self.x_steps + int(self.x_error))
         self.x_error -= int(self.x_error)
