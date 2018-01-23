8bc87e1ea71144709b08abf17148af24e17d8105
Ganesh @ PCTEST <35346980+garva-pctest@users.noreply.github.com>
2018-01-23 11:38:07 -0500

    attempting to return values

--- data_entry_gui.py
+++ data_entry_gui.py
@@ -11,22 +11,31 @@ class DataEntryGUI(tk.Tk):
     def __init__(self, parent):
         tk.Tk.__init__(self, parent)
         self.parent = parent
-        self.value = 0
         self.setup()
 
     def setup(self):
-        label = tk.Label(self, text='Please enter the desired value for this point.', padx=10, pady=10)
-        label.grid(row=0, column=0)
-        entry = tk.Entry(self)
-        entry.grid(row=0,column=1, padx=10, pady=10)
+        label = tk.Label(self, text='Please enter the desired value: ')
+        label.grid(row=0, column=0, padx=10, pady=10)
+        self.sv = tk.StringVar()
+        self.sv.trace("w", lambda name, index, mode, sv=self.sv: self.callback(self.sv))
+        self.entry = tk.Entry(self, textvariable=self.sv)
+        self.entry.grid(row=0,column=1, padx=10, pady=10)
         self.grid_rowconfigure(1)
         self.grid_columnconfigure(2)
+
+    def callback(self, sv):
+        return sv
+
+    def getval(self):
+        return self.value
 
 
 def main():
     man = DataEntryGUI(None)
     man.title('Data Entry')
     man.mainloop()
+    print man.getval()
 
 
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+    main()
