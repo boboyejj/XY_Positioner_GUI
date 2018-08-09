e0c2dfe434f0e708fb9e8fb7aa43e744e9128263
changhwan95 <changhwan95@hotmail.com>
2018-08-09 11:25:58 -0700

    added more skeleton to the init function of the mainframe

--- xy_positioner_gui.py
+++ xy_positioner_gui.py
@@ -3,13 +3,25 @@
     changhwan.choi@pctest.com
 """
 
-import numpy as np
 from src.grid_scan_manual import run_scan, generate_grid, move_to_pos_one
 from src.motor_driver import MotorDriver
 from src.location_select_gui import LocationSelectGUI
 from src.manual_gui_grid import ManualGridGUI
+import numpy as np
+import wx
 
 class MainFrame(wx.Frame):
-    def __init__(self):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(800, 600))
 
+        # Variables
 
+        # Accelerator Table/Shortcut Keys
+
+        # UI Elements
+
+        # Menu Bar
+
+        # Sizers/Layout, Static Lines & Static Boxes
+
+        
