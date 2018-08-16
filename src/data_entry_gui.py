3bb9f84c3e98860a0e4e9e19bec0259afff74d31
changhwan95 <changhwan95@hotmail.com>
2018-08-16 12:34:20 -0700

    Revert "deleted some unused files"
    This reverts commit 819e0b93a380b0f99161c4b7d8be88055e68e12f.

---
+++ src/data_entry_gui.py
@@ -0,0 +1,61 @@
+"""
+    Created by Ganesh Arvapalli on 1/22/18
+    ganesh.arvapalli@pctest.com
+"""
+
+import tkinter as tk
+import sys
+
+
+class DataEntryGUI(tk.Tk):
+
+    def __init__(self, parent):
+        tk.Tk.__init__(self, parent)
+        self.parent = parent
+        self.value = 0.0
+        self.setup()
+
+    def setup(self):
+        label = tk.Label(self, text='Please enter the desired value: ')
+        label.grid(row=0, column=0, padx=10, pady=10)
+        sv = tk.StringVar()
+        sv.trace("w", lambda name, index, mode, sv=sv: self.callback(sv))
+        entry = tk.Entry(self, textvariable=sv)
+        entry.focus()
+        entry.grid(row=0,column=1, padx=10, pady=10)
+        self.grid_rowconfigure(1)
+        self.grid_columnconfigure(2)
+
+        self.bind('<Return>', self.exit_gui)
+
+    def callback(self, sv):
+        try:
+            self.value = float(sv.get())
+        except ValueError:
+            print("Please type a valid number.")
+
+    def getval(self):
+        return self.value
+
+    def exit_gui(self, event):
+        self.destroy()
+
+
+def main():
+    man = DataEntryGUI(None)
+    man.title('Data Entry')
+    man.mainloop()
+    print(man.getval())
+
+    man = DataEntryGUI(None)
+    man.title('Data Entry')
+    man.mainloop()
+    print(man.getval())
+
+    man = DataEntryGUI(None)
+    man.title('Data Entry')
+    man.mainloop()
+    print(man.getval())
+
+if __name__ == '__main__':
+    main()
