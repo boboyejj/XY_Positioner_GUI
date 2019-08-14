b2b8e5ba823f27b3229b80606a120682fe447c3c
Jiejia Ye <boboye@terpmail.umd.edu>
2019-08-14 13:24:07 -0400

    Add file handler and console handler

--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,12 @@ logger = logging.getLogger('user')
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s:%(message)s')
 
+# handle info to a log file
 file_handler = logging.FileHandler("NS Testing.log")
-file_handler.setFormatter(formatter)
-
+#file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
+
+# handle info to the sys.stdout
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(formatter)
+logger.addHandler(consoleHandler)
