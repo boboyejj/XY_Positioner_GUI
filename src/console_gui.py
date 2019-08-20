bcb6f29d4244ef471373460f36b09e7ec346bd6d
Jiejia Ye <boboye@terpmail.umd.edu>
2019-08-20 15:33:05 -0400

    Let console float on the top

--- src/console_gui.py
+++ src/console_gui.py
@@ -13,6 +13,8 @@ Chang Hwan 'Oliver' Choi, Biomedical/Software Engineering Intern (Aug. 2018) - c
 """
 
 import wx
+import keyboard
+import time
 
 
 class TextRedirector(object):
@@ -44,11 +46,26 @@ class ConsoleGUI(wx.Frame):
         :param title: Title for the GUI window.
         """
         wx.Frame.__init__(self, parent, title=title, size=(800, 700))
+        self.SetWindowStyle(wx.CAPTION | wx.CLOSE_BOX | wx.STAY_ON_TOP)
 
         # UI Elements
         self.console_text = wx.StaticText(self, label="Console Output")
         self.console_text.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL, wx.BOLD))
         self.console_tctrl = wx.TextCtrl(self, size=(600, 400), style=wx.TE_MULTILINE | wx.TE_READONLY)
+
+        # Menu Bar
+        pause_id = 120
+        menubar = wx.MenuBar()
+        helpmenu = wx.Menu()
+        pause_item = wx.MenuItem(helpmenu, pause_id, text="Pause", kind=wx.ITEM_NORMAL)
+        helpmenu.Append(pause_item)
+        menubar.Append(helpmenu, 'Help')
+        self.Bind(wx.EVT_MENU, self.pauseProg, id=pause_id)
+        self.SetMenuBar(menubar)
+
+        # Shortcut
+        self.accel_tbl = wx.AcceleratorTable([(wx.ACCEL_CTRL, ord('p'), pause_id)])
+        self.SetAcceleratorTable(self.accel_tbl)
 
         # Sizers
         self.mainsizer = wx.BoxSizer(wx.VERTICAL)
@@ -61,6 +78,22 @@ class ConsoleGUI(wx.Frame):
         self.SetSizer(self.mainsizer)
         self.SetAutoLayout(True)
         self.mainsizer.Fit(self)
+
+    def pauseProg(self,e):
+        """
+        pause the program
+        """
+        print("press Enter to continue")
+
+        while True:
+            key = keyboard.read_key()
+
+            if key == "enter":
+                print("Program is resumed")
+                # exit(0)
+                break
+
+            time.sleep(0.5)
 
 if __name__ == '__main__':
     consolegui = wx.App()
